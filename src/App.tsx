02e955e7379b88f098ed67e8fe13be763ad6cea2
OpArcade <oparcade10@gmail.com>
2024-07-26 16:48:09 +0530

    files set and working start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Events from './pages/Events';
+import Sponsor from './pages/Sponsor';
+
+
 
 function App() {
   return (
-    
-    <h1>MATRIX 2.0</h1>
-    
+    <BrowserRouter>
+      <MainDiv>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Home' element={ <Home />} />
+          <Route path='/Events' element={ <Events /> } />
+          <Route path='/Sponsors' element={<Sponsor />} />
+        </Routes>
+      </MainDiv>
+      <h1>Hello</h1>
+    </BrowserRouter>
+
   );
 }
 
+const MainDiv = styled.div`
+
+`;
 export default App;
---
+++ src/components/Glimpses.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Glimpses = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Glimpses
