ca4b14caaa5fee7940dff587ce825dce56e87770
OpArcade <oparcade10@gmail.com>
2024-08-08 14:02:33 +0530

    chnage

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/Navbar' element = {<Navbar />} />
           <Route path='/Burger' element = { <Burger />} />
-          {/* <Route path='/Home' element={ <Home />} /> */}
+          {/* <Route path='/Home' element={ <Home />} />` */}
           <Route path='/Events' element={ currentUser !== null ? <Events /> : <Home/> } />
           <Route path='/Sponsors' element={<Sponsor />} />
           <Route path='/Contact' element = { <Contact /> } />
