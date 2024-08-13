c21df2564931944a1215beabde3fac189b0f0a4b
Ashutosh Kumar <132209941+AshutoshStark@users.noreply.github.com>
2024-08-13 11:38:23 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,12 +14,12 @@ const Navbar = () => {
   const [paymentdetails , setPaymentdetails] = useState<any>({});
 
   const getPaymentdetails=()=>{
-    if (currentUser.uid){
+    if (currentUser?.uid){
       const dbref = ref(db,`payments/${currentUser.uid}`)
     
     onValue(dbref,(snapshot)=>{
-      if(snapshot.exists()){
-        let info = snapshot.val();
+      if(snapshot?.exists()){
+        let info = snapshot?.val();
         setPaymentdetails(info)
       }
     })
